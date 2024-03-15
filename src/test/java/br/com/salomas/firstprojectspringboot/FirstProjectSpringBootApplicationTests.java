5a64bfbe552c5d6aafceda712c42f63ebc284427
Salomas <salomas.oficial@gmail.com>
2024-03-14 22:46:44 -0300

    initialazing a project in spring boot

---
+++ src/test/java/br/com/salomas/firstprojectspringboot/FirstProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salomas.firstprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
